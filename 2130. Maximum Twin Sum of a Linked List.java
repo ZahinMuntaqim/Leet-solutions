68c63c537173fb97d520ab7004b4306ff43b4ff3
ZAHIN MUNTAQIM <thesis412@gmail.com>
2026-02-18 02:43:12 +0600

    Create 2130. Maximum Twin Sum of a Linked List.java

---
+++ 2130. Maximum Twin Sum of a Linked List.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+         // Step 1: Find middle of linked list
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        // Step 2: Reverse second half
+        ListNode prev = null;
+        while (slow != null) {
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+        
+        // Step 3: Calculate maximum twin sum
+        int maxSum = 0;
+        ListNode first = head;
+        ListNode second = prev;  // head of reversed second half
+        
+        while (second != null) {
+            maxSum = Math.max(maxSum, first.val + second.val);
+            first = first.next;
+            second = second.next;
+        }
+        
+        return maxSum;
+    }
+}
